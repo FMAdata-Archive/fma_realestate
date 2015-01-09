c3aadd2f3d516dc30fc35380ccb4673ee9ebecf8
Blaine Johnson <blainejohnson17@gmail.com>
2015-01-09 12:48:13 -0700

    adds fixture for Cass#tiger

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,2 +1,17 @@
 $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
+require 'rspec'
 require 'fma_realestate'
+
+Dir["./spec/support/**/*.rb"].sort.each { |f| require f }
+
+RSpec.configure do |config|
+  config.include RequestFixturesHelper
+
+  config.before(:all) do
+    Excon.defaults[:mock] = true
+  end
+
+  config.after(:each) do
+    Excon.stubs.clear
+  end
+end
