1cd1435c7baff691d6ec70ee08be6888b3f67f18
Blaine Johnson <blainejohnson17@gmail.com>
2015-01-09 16:56:57 -0700

    Adds spec to public_record_spec

--- spec/fma_realestate/public_record_spec.rb
+++ spec/fma_realestate/public_record_spec.rb
@@ -9,9 +9,18 @@ describe FmaRealestate::PublicRecord do
     context "with :raise_errors => false" do
       let(:authenticated_client) { described_class.new(:access_token => 'test', :raise_errors => false) }
 
-      it 'raises authentication error with invalid access_token' do
-        Excon.stub({ :method => :get, :path => '/api/data_quick_files/search_by_address', :headers => { 'Authorization' => "Token token=test" }}, {:status => 401 })
+      it 'suppresses request error' do
+        connection = double("connection")
+        allow(connection).to receive(:request) { raise FmaRealestate::RequestError }
+        authenticated_client.instance_variable_set(:@connection, connection)
         expect {authenticated_client.search_by_address()}.to_not raise_error
+      end
+
+      it "doesn't suppress non request errors" do
+        connection = double("connection")
+        allow(connection).to receive(:request) { raise StandardError }
+        authenticated_client.instance_variable_set(:@connection, connection)
+        expect {authenticated_client.search_by_address()}.to raise_error
       end
     end
 
