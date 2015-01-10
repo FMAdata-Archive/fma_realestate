72b5a2e3e129d9f308bebe37860f2feaff04b0a5
Blaine Johnson <blainejohnson17@gmail.com>
2015-01-09 17:51:00 -0700

    Implements PublicRecord#search_by_advanced

--- lib/fma_realestate/public_record.rb
+++ lib/fma_realestate/public_record.rb
@@ -3,5 +3,9 @@ module FmaRealestate
     def search_by_address(params={})
       get('/api/data_quick_files/search_by_address', params)
     end
+
+    def search_by_advanced(params={})
+      get('/api/data_quick_files/search_by_advanced', params)
+    end
   end
 end
