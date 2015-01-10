f641f92cd63749e29a229765d83e1d7dd19c4ebf
Blaine Johnson <blainejohnson17@gmail.com>
2015-01-09 17:27:17 -0700

    Using shared_examples in cass_spec

--- spec/fma_realestate/cass_spec.rb
+++ spec/fma_realestate/cass_spec.rb
@@ -1,45 +1,34 @@
 require 'spec_helper'
 
 describe FmaRealestate::Cass do
+  let(:authenticated_client) { described_class.new(:access_token => 'test', :raise_errors => true) }
+  let(:anonymous_client) { described_class.new(:raise_errors => true) }
   
   describe ".tiger" do
     context "with :raise_errors => false" do
-      it 'raises authentication error with invalid access_token' do
-        Excon.stub({ :method => :get, :path => '/api/cass/tiger', :headers => { 'Authorization' => "Token token=test" }}, {:status => 401 })
-        client = FmaRealestate::Cass.new(:access_token => 'test', :raise_errors => false)
-        expect {client.tiger()}.to_not raise_error
+      let(:authenticated_client) { described_class.new(:access_token => 'test', :raise_errors => false) }
+
+      it_behaves_like "handles error suppression" do
+        let(:action) { authenticated_client.tiger }
       end
     end
 
     context "with :raise_errors => true" do
-      it 'raises authentication error with invalid access_token' do
-        Excon.stub({ :method => :get, :path => '/api/cass/tiger', :headers => { 'Authorization' => "Token token=test" }}, {:status => 401 })
-        client = FmaRealestate::Cass.new(:access_token => 'test', :raise_errors => true)
-        expect {client.tiger()}.to raise_error(FmaRealestate::AuthenticationError)
+
+      it_behaves_like "requires valid authentication" do
+        let(:action) { anonymous_client.tiger }
       end
 
-      it 'raises authentication error with missing access_token' do
-        Excon.stub({ :method => :get, :path => '/api/cass/tiger'}, {:status => 401 })
-        client = FmaRealestate::Cass.new(:raise_errors => true)
-        expect {client.tiger()}.to raise_error(FmaRealestate::AuthenticationError)
+      it_behaves_like "handles 404 response" do
+        let(:action) { authenticated_client.tiger }
       end
 
-      it 'raises not found error with 404 response' do
-        Excon.stub({ :method => :get, :path => '/api/cass/tiger'}, {:status => 404 })
-        client = FmaRealestate::Cass.new(:raise_errors => true)
-        expect {client.tiger()}.to raise_error(FmaRealestate::ResourceNotFound)
+      it_behaves_like "handles 400 response" do
+        let(:action) { authenticated_client.tiger }
       end
 
-      it 'raises bad request error with 400 response' do
-        Excon.stub({ :method => :get, :path => '/api/cass/tiger'}, {:status => 400 })
-        client = FmaRealestate::Cass.new(:raise_errors => true)
-        expect {client.tiger()}.to raise_error(FmaRealestate::BadRequest)
-      end
-
-      it 'raises http error with unexpected error response' do
-        Excon.stub({ :method => :get, :path => '/api/cass/tiger'}, {:status => 403 })
-        client = FmaRealestate::Cass.new(:raise_errors => true)
-        expect {client.tiger()}.to raise_error(FmaRealestate::HTTPError)
+      it_behaves_like "handles 400 response" do
+        let(:action) { authenticated_client.tiger }
       end
     end
 
@@ -47,9 +36,8 @@ describe FmaRealestate::Cass do
       let(:fixture) { request_fixture('tiger') }
       it "should get response" do
         Excon.stub({ :method => :get, :path => '/api/cass/tiger', :headers => { 'Authorization' => "Token token=test" }}, {:status => 200, :body => fixture })
-        client = FmaRealestate::Cass.new(:access_token => 'test', :raise_errors => true)
         params = {:street_address => "1325 Pearl Street", :city => "Boulder", :state => "CO", :zip => "80302"}
-        response = client.tiger(params)
+        response = authenticated_client.tiger(params)
         expect(response.keys.count).to eq(28)
         expect(response['return_code']).to eq('1')
         expect(response['dpv_confirmation_indicator']).to eq('Y')
